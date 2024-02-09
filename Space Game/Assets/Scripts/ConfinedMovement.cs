510fc3d486b99a7463968268ca8dcfc2d568dbba
Can McKayra <candilekli@hotmail.com>
2024-02-09 09:17:20 +0300

    Update ConfinedMovement.cs

--- Space Game/Assets/Scripts/ConfinedMovement.cs
+++ Space Game/Assets/Scripts/ConfinedMovement.cs
@@ -7,7 +7,7 @@ public class ConfinedMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+        
     }
 
     // Update is called once per frame
